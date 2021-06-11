87caae9ddb4217ff02fb93bbc148bef48e3bb7c8
unknown <ashiwn.menon202@gmail.com>
2021-06-11 22:23:58 +0530

    Added tabs for event categories

---
+++ src/components/Home.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+const event_category = [
+    {
+      name:'All Events',
+      value:'ALL_EVENTS'
+    },
+    {
+        name:'Webinars',
+        value:'WEBINAR'
+    },
+    {
+        name:'Coding Events',
+        value:'CODING_EVENT'
+    },
+    {
+        name:'Bootcamp events',
+        value:'BOOTCAMP_EVENT'
+    },
+    {
+        name:'Workshop',
+        value:'WORKSHOP'
+    },    
+  ]
+
+//sample api : https://api.codingninjas.com/api/v3/events?event_category=ALL_EVENTS&event_sub_category=Upcoming&tag_list=Career%20Guidance,Web%20Development&offset=1
+
+function Event({value}){
+    console.log(value)
+    return(<><h1>{value}</h1></>)
+}
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    width: '95%',
+    backgroundColor: theme.palette.background.paper,
+    padding:'1%',
+  },
+}));
+
+export default function Home() {
+
+  const classes = useStyles();
+  const [value, setValue] = React.useState('ALL_EVENTS');
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="scrollable"
+          scrollButtons="auto"
+          aria-label="scrollable auto tabs example"
+        >
+        {
+            event_category.map(event =>(
+                <Tab label={event.name} value={event.value} key={event.value}/>
+            ))
+        }
+        </Tabs>
+      </AppBar>
+      <Event value={value}/>
+    </div>
+  );
+}
